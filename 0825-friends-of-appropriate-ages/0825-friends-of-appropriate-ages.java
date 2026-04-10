d257dee23cc9493b108bd4a2bd8ceaee9a080d7c
Himanshuchoudhary81 <him.0209200@gmail.com>
2026-04-10 14:54:18 +0530

    Time: 2570 ms (5.42%), Space: 48.6 MB (33.99%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.java
@@ -6,6 +6,7 @@ class Solution {
         for(int i=ages.length-1;i>0;i--){
             for(int j=i-1;j>=0;j--){
                 if(ages[j] <= (double)0.5*ages[i]+7) break;
+                if(ages[j] > ages[i]) break ;
                 if(ages[j] > 100 && ages[i] < 100) break ;
                 if(ages[i]==ages[j]) res+=2;
                 else res++;
